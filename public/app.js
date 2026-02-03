da8d00bb9c4601044a6cf2ff70b382c001b07355
Nawin chander <nawinnapzy289@gmail.com>
2026-02-03 19:12:28 +0530

    add the files

--- public/app.js
+++ public/app.js
@@ -1,3 +1,32 @@
-const path = require("path");
+const API = "http://localhost:5000/api";
 
-app.use(express.static(path.join(__dirname, "../public")));
+// MongoDB API Call
+async function createMongoUser() {
+  const name = document.getElementById("mongoName").value;
+  const email = document.getElementById("mongoEmail").value;
+
+  const res = await fetch(`${API}/mongo-user`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email })
+  });
+
+  const data = await res.json();
+  document.getElementById("response").innerText =
+    "MongoDB User Created: " + JSON.stringify(data);
+}
+
+// MySQL API Call
+async function createSQLUser() {
+  const name = document.getElementById("sqlName").value;
+  const email = document.getElementById("sqlEmail").value;
+
+  const res = await fetch(`${API}/mysql-user`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email })
+  });
+
+  const text = await res.text();
+  document.getElementById("response").innerText = text;
+}
